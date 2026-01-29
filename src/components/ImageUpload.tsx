b75a11f871c102feb68758b3c120030d71c07b6d
szabo <szabo.balazs1214@gmail.com>
2026-01-29 11:25:40 +0100

    imageUpload kesz

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -2,24 +2,35 @@ import { useState } from "react";
 
 const ImageUpload = () => {
   const [imagePath, setImagePath] = useState<string>("");
+  const [fileSelected, setFileSelected] = useState<boolean>(false);
   return (
     <div
-      id="imageUploadDiv"
+      className={`imageUploadDiv ${fileSelected ? "has-file" : ""}`}
       onClick={() => document.getElementById("imageUpload")?.click()}
     >
-      <p>Kattints ide a kép feltöltéséhez</p>
-      <input
-        type="file"
-        id="imageUpload"
-        // accept="image/png, image/jpeg"
-        style={{ display: "none" }}
-        onChange={(e) => {
-          if (e.target.files && e.target.files[0]) {
-            const file = e.target.files[0];
-            setImagePath(file.name);
-            console.log(file.name);
-          }
-        }}
+      <div style={{ display: fileSelected ? "none" : "block" }}>
+        <p>Kattints ide a kép feltöltéséhez</p>
+        <input
+          type="file"
+          id="imageUpload"
+          // accept="image/png, image/jpeg"
+          style={{ display: "none" }}
+          onChange={(e) => {
+            const files = e.target.files;
+            if (files && files.length > 0) {
+              const file = files[0];
+              const objectUrl = URL.createObjectURL(file);
+              setImagePath(objectUrl);
+              setFileSelected(true);
+            }
+          }}
+        />
+      </div>
+      <img
+        src={imagePath}
+        alt="Preview"
+        id="imagePreview"
+        style={{ display: fileSelected ? "block" : "none" }}
       />
     </div>
   );
