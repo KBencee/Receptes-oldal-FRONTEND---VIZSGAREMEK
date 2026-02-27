f763465e9d28b2aee3b8c3db26847bacafb2c6ac
szabo <szabo.balazs1214@gmail.com>
2026-02-27 13:40:06 +0100

    pfp upload

--- src/components/ProfileHeadBar.tsx
+++ src/components/ProfileHeadBar.tsx
@@ -2,10 +2,13 @@ import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useQuery } from "@tanstack/react-query";
 import SettingsTab from "./SettingsTab";
+import { useContext } from "react";
+import { AuthUserContext } from "../context/AuthenticatedUserContextProvider";
 
 const API_BASE_URL = "https://cbnncff2-7114.euw.devtunnels.ms";
 
 const ProfileHeadBar = () => {
+  const authUser = useContext(AuthUserContext)
   const { data: user } = useQuery({
     queryKey: ["me"],
     queryFn: async () => {
@@ -28,18 +31,30 @@ const ProfileHeadBar = () => {
   };
   return (
     <div className="headBar profileHeadBar">
-      <h1 className="profileName">{user?.username}'s recipes</h1>
+      <h1 className="profileName">{localStorage.getItem("username")} receptjei</h1>
       {/* <img src={user?.profileImageUrl} alt={user?.username} className="profileImage"/> */}
-      <button className="settingsButton" title="Settings" onClick={() => {
-        const overlay = document.getElementById("alertOverlay");
-        if (overlay) {
-          overlay.style.display = "flex";
-        }
-      }}>
+      <button
+        className="settingsButton"
+        title="Settings"
+        onClick={() => {
+          const overlay = document.getElementById("alertOverlay");
+          if (overlay) {
+            overlay.style.display = "flex";
+          }
+        }}
+      >
         <FontAwesomeIcon icon={faGear} />
       </button>
-      <div id="alertOverlay" onClick={handleOverlayClick} style={{ display: "none" }}>
-        <SettingsTab imageUrl={user?.profileImageUrl || ""} username={user?.username || "User"} onClick={() => alert("Settings saved!")} />
+      <div
+        id="alertOverlay"
+        onClick={handleOverlayClick}
+        style={{ display: "none" }}
+      >
+        <SettingsTab
+          imageUrl={authUser?.authUser.profileImageUrl || user?.profileImageUrl || "profile.webp"}
+          username={authUser?.authUser.username || user?.username || "User"}
+          reloadFunc={() => window.location.reload()}
+        />
       </div>
     </div>
   );
--- src/components/SettingsTab.tsx
+++ src/components/SettingsTab.tsx
@@ -1,16 +1,78 @@
-import ImageUpload from "./ImageUpload"
+import ImageUpload from "./ImageUpload";
 
-const SettingsTab = (props: { imageUrl: string, username: string, onClick: () => void }) => {
+const API_BASE_URL = "https://cbnncff2-7114.euw.devtunnels.ms/api";
+const token = localStorage.getItem("access");
+
+const editUsername = (props: { username: string; reloadFunc: () => void }) => {
+  fetch(`${API_BASE_URL}/Auth/me/username`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      newUsername: props.username,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Felhasználónév módosítása sikertelen");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      if (data.username != localStorage.getItem("username")) {
+        localStorage.setItem("username", data.username);
+        alert("Profil sikeresen módosítva!");
+        props.reloadFunc();
+        document.getElementById("alertOverlay")!.style.display = "none";
+      }
+    })
+    .catch((error) => {
+      console.error("Felhasználónév módosítása sikertelen:", error);
+    });
+};
+
+const editProfilePicture = (props: {
+  image: string;
+  reloadFunc: () => void;
+}) => {};
+
+const cancelButtonHandler = () => {
+  const overlay = document.getElementById("alertOverlay");
+  if (overlay) {
+  }
+};
+
+const SettingsTab = (props: {
+  imageUrl: string;
+  username: string;
+  reloadFunc: () => void;
+}) => {
   return (
     <div className="settingsTab">
       {/* <img src={props.imageUrl} className="profileImage"/>
        */}
-       <ImageUpload image={props.imageUrl} setImage={() => {}} />
-      <h1 contentEditable>{props.username}</h1>
-        <button onClick={props.onClick}>Mentés</button>
-        <button onClick={props.onClick}>Mégse</button>
+      <ImageUpload image={props.imageUrl} setImage={() => {}} />
+      <h1 contentEditable id="usernameEdit">
+        {props.username}
+      </h1>
+      <button
+        onClick={() =>
+          editUsername({
+            username:
+              (document.getElementById("usernameEdit") as HTMLElement)
+                ?.innerText || props.username,
+            reloadFunc: props.reloadFunc,
+          })
+        }
+      >
+        Mentés
+      </button>
+      <button onClick={cancelButtonHandler}>Mégse</button>
     </div>
-  )
-}
+  );
+};
 
-export default SettingsTab
+export default SettingsTab;
